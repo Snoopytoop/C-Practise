9bc3a6211bd93a35b1ea368dc76f3f988d280117
Samuel Luke <108830145+Snoopytoop@users.noreply.github.com>
2025-12-26 17:19:41 +0000

    Create add2nums.c
    practise adding 2 numbers

---
+++ add2nums.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num1;
+    int num2;
+
+    printf("Enter your first number: ");
+    scanf("%d", &num1);
+    printf("Enter your second mumber: ");
+    scanf("%d", &num2);
+    printf("The sum of your two numbers is %d\n", num1 + num2);
+
+    return 0;
+}
